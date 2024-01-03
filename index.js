e4b900a6875676b1dc7f07ab4fd1d6c058b8c66d
ViGenereux <genereux.vincent@gmail.com>
2024-01-03 18:20:35 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./src/app";
-
-const rootNode = document.getElementById("root");
-const root = createRoot(rootNode);
-
-root.render(<App />);
